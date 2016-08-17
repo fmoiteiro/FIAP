21f7fcc1db6df3545071187c6968a0088f164bfb
fmoiteiro <felipemoiteiro@gmail.com>
2016-08-17 11:42:06 -0300

    Add files via upload

---
+++ Script_start_mongo.bat
@@ -0,0 +1,6 @@
+
+mkdir C:\opensource\MongoDB-3.0\bin\data
+
+start C:\opensource\MongoDB-3.0\bin\mongod --dbpath "C:\opensource\MongoDB-3.0\bin\data"
+
+start C:\opensource\MongoDB-3.0\bin\mongo.exe
